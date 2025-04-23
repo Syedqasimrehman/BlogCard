72d80c77918c6b55d91a4666a8dd33e365551b78
syed qasim <qasimsyed563@gmail.com>
2025-04-23 20:28:50 +0500

    First Commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./card.css";
+const Card = () => {
+  return (
+    <div className="card_design">
+      <div className="img-dev">
+        <img src="./src/assets/cardImg.jpg" alt="cardimg" />
+      </div>
+
+      <div className="content-data">
+        <div className="content-dev">
+          <span className="content-cetg">Interior</span>
+          <h3 className="cont-heading">Top 5 Living Room Inspirations</h3>
+        </div>
+        <div className="content-desc">
+          <p>
+            Curated vibrats colors for your living, make it pop & calm in the
+            same time.
+          </p>
+        </div>
+        <a className="btn" href="#">
+          Read more
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
